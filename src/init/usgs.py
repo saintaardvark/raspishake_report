f299de7ff874f093443dfce680ba72f25ff72375
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2023-06-11 17:40:54 -0700

    Change to week for usgs report
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- src/init/usgs.py
+++ src/init/usgs.py
@@ -4,23 +4,24 @@ from datetime import datetime
 import json
 import requests
 
-URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson"
-
+LAST_DAY_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson"
+LAST_WEEK_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
 
 def main():
     """
     Main entry point
     """
-    resp = requests.get(URL)
+    resp = requests.get(LAST_WEEK_URL)
     quakes = resp.json()
-    print(json.dumps(quakes, indent=2))
+    # print(json.dumps(quakes, indent=2))
 
     # TODO: We should be parsing this w/geojson
     for quake in quakes["features"]:
         lon_e, lat_e, depth = quake["geometry"]["coordinates"]
         mag = quake["properties"]["mag"]
         time_e = quake["properties"]["time"] / 1000  # ms since epoch
-        time_e_formatted = datetime.fromtimestamp(time_e).strftime("%Y-%m-%dT%H:%M:%S")
+        time_e_formatted = datetime.utcfromtimestamp(time_e).strftime("%Y-%m-%dT%H:%M:%S")
+
         event_id = quake["properties"]["code"]
         location = quake["properties"]["place"]
         url = quake["properties"]["url"]
