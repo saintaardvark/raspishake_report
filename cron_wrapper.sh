b4ec0e00f89f05528c3952972135a6018334fd66
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-05-19 07:07:03 -0700

    Update cron_wrapper.sh

--- cron_wrapper.sh
+++ cron_wrapper.sh
@@ -3,7 +3,14 @@
 cd /backup/batch_jobs/raspishake_report/
 source .venv/bin/activate
 
+echo "Worldwide quakes > 4.5:"
+echo
 ./src/usgs.py pretty_table
+
+echo
+echo "Nearby quakes > 1.0:"
+echo
+./src/usgs.py pretty_table --feed LAST_DAY_OVER_1_POINT_0 --radius 1000
 
 # Turning this off while Raspishake servers are being rebuilt
 # echo
